1d2a59bbf6aaca4e90b622b129e9af78704a4a1f
Mateusz <mateh2304@gmail.com>
2024-01-15 23:22:16 +0100

    sila skoku, blok mv, testowanie zbierania punktow

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemsCollection : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI CollectedTXT;
+
+    //Zmienna do zliczania podniesionych itemkow
+    private int collectedItems = 0;
+
+    //Triger gdy player wejdzie w Tag collectable
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("collectable"))
+        {
+            Destroy(collision.gameObject);
+            collectedItems++;
+            //Zmiana tekstu w Canvas -> collectedTXT (odwo³anie ustawione w Unity)
+            CollectedTXT.text = "Zebrane itemki: " + collectedItems.ToString();
+        }
+    }
+}
