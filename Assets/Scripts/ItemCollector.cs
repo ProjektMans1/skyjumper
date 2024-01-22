d3db28ad461846dc0f456965dada583b11805ff2
Mateusz <mateh2304@gmail.com>
2024-01-22 21:06:15 +0100

    testowanie textMeshPro pod dialogi

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -13,12 +13,14 @@ public class ItemsCollection : MonoBehaviour
     //Zmienna do zliczania podniesionych itemkow
     private int collectedItems = 0;
 
+
     //Triger gdy player wejdzie w Tag collectable
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("collectable"))
         {
             Destroy(collision.gameObject);
+
             collectedItems++;
             //Zmiana tekstu w Canvas -> collectedTXT (odwo³anie ustawione w Unity)
             CollectedTXT.text = "Zebrane itemki: " + collectedItems.ToString();
