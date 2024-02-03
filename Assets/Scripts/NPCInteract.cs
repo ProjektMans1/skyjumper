1c6fe63862a91f7bac66e25a711c347185ae512e
Mateusz <mateh2304@gmail.com>
2024-02-03 13:56:44 +0100

    dialog

--- Assets/Scripts/NPCInteract.cs
+++ Assets/Scripts/NPCInteract.cs
@@ -2,18 +2,37 @@ using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCInteract : MonoBehaviour
 {
     public bool see;
+    public TextMeshProUGUI dialogue;
+    public Image image;
+
+    void Start()
+    {
+        dialogue.enabled = false;    
+        image.enabled = false;    
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("npc"))
+        if (collision.gameObject.CompareTag("Player"))
         {
+            dialogue.enabled = true;
+            image.enabled = true;
             
         }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        dialogue.enabled = false;
+        image.enabled = false;
     }
 
 }
