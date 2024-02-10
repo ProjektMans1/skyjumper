de5e443fab562e1408a02eae6e000309f52b1af4
Mateusz <mateh2304@gmail.com>
2024-02-10 17:31:54 +0100

    Final

--- Assets/Scripts/menuControl.cs
+++ Assets/Scripts/menuControl.cs
@@ -8,7 +8,35 @@ public class menuControl : MonoBehaviour
     
    public void PlayGame()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGameAfterEnd()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + -4);
+
+    }
+
+    public void PlayGameLevel1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void PlayGameLevel2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void PlayGameLevel3()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + -2);
+
+    }
+    public void PlayGameLevel4()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + -3);
+
     }
 
     public void QuitGame()
